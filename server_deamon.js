3280bc4d3b9bcb029273893367db5c603c6e2729
Андрей <scale_@mail.ru>
2019-11-11 16:58:55 +0500

    ...

---
+++ server_deamon.js
@@ -0,0 +1,2 @@
+let child_process = require("child_process");
+child_process.exec("ts-node server.tsx");
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
 	entry: {
 		index: path.resolve(__dirname, './src/ts/index.tsx'),
+		
 	},
 	output: {
 		filename: '[name].js',
