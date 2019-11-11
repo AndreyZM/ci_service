f48410d1d157d9a566ed538904db9b763cb3e7e1
Андрей <scale_@mail.ru>
2019-11-11 17:04:00 +0500

    ....

--- server_deamon.js
+++ server_deamon.js
@@ -1,2 +1,4 @@
 let child_process = require("child_process");
-child_process.exec("ts-node server.tsx");
+let prc = child_process.exec("ts-node server.tsx");
+prc.stdout.on("data", (chunk) => process.stdout.write(chunk));
+prc.stderr.on("data", (chunk) => process.stderr.write(chunk));
