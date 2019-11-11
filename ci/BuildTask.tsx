ab97b4033278d58e88986583a4b2077f99995e96
Андрей <scale_@mail.ru>
2019-11-12 02:39:27 +0500

    ....

--- ci/BuildTask.tsx
+++ ci/BuildTask.tsx
@@ -108,7 +108,7 @@ export class BuildTask
 				stdio: ["pipe", out, out]
 			});
 		let process = wrap(p);
-		script.split("\n").forEach((chunk) => p.stdin.end(chunk));
+		script.split("\n").forEach((chunk) => p.stdin.write(chunk));
 		this.terminator = () => process.terminate();
 		return process.wait;
 	}
