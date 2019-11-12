0863c18bf20ef52871a7f04a8d7193274d123147
Андрей <scale_@mail.ru>
2019-11-12 12:14:56 +0500

    ...

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Collapse, Intent, Label, Pre, Spinner, Tag, Divider } from "@blueprintjs/core";
+import { Button, Card, Collapse, Intent, Label, Pre, Spinner, Tag, Divider, H3, UL, Blockquote } from "@blueprintjs/core";
 import React = require("react");
 
 import "@blueprintjs/core/lib/css/blueprint.css";
@@ -7,6 +7,7 @@ import "normalize.css";
 import { BuildTask } from "../../ci/BuildTask";
 import { Api, API } from "./api/Api";
 import { Async } from "./utils/Async";
+import { BLOCKQUOTE } from "@blueprintjs/core/lib/esm/common/classes";
 
 export class App extends React.Component
 {
@@ -39,17 +40,22 @@ export class TaskView extends React.Component<{ task: BuildTask }, { showLogs: b
 		};
 
 		return <Card>
-			<h3>{`Task #${this.props.task.id} ${this.props.task.project}/${this.props.task.revision}`}</h3>
-			<p>Status: <Tag intent={statusIntents[this.props.task.status]}>{this.props.task.status}</Tag></p>
+			<H3>{`Task #${this.props.task.id} ${this.props.task.project}/${this.props.task.revision}`}<Tag intent={statusIntents[this.props.task.status]}>{this.props.task.status}</Tag></H3>
 			<Button onClick={() => this.setState((state) => ({ showLogs: !state.showLogs }))}>
-				{this.state.showLogs ? "Hide" : "Show"} build logs
+				{this.state.showLogs ? "Hide" : "Show"} logs
 			</Button>
 			<Collapse isOpen={this.state.showLogs} >
 				<Pre style={{ whiteSpace: "pre-line" }}>
 					<iframe src={this.props.task.logPath}/>
 				</Pre>
 			</Collapse>
+				<Blockquote>
+					<UL>
+					{this.props.task.commits.map((commit) => <li>{commit.branch + ":" + commit.author}<br/>{commit.message}</li>)}
+					</UL>
+				</Blockquote>
 			<Divider />
+			
 			{this.props.task.status === "running" && <Button intent={Intent.DANGER} onClick={() => API.taskkill({ id: this.props.task.id })}>Stop</Button>}
 			{this.props.task.status === "completed" && <Button intent={Intent.PRIMARY} onClick={() => window.open(this.props.task.runUrl, "_blank")}>Run</Button>}
 			{this.props.task.status === "failed" && <Button intent={Intent.WARNING} >Restart</Button>}
